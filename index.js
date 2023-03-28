4d78584e44cbf2db0844d175091c8b0c9c27eb2f
Savannah <skruse101@gmail.com>
2023-03-27 18:03:05 -0700

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result  
+  if(feet <= 400) {
+    result = "This one is on me!";
+  }
+  if (feet > 400 && feet <= 2000){
+    result = "That will be twenty bucks.";
+  }
+  if (feet > 2000 && feet <= 2500){
+    result = "I will gladly take your thirty bucks."
+  }
+  else if (feet > 2500){
+    return "No can do."
+  }
+  return result
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(location){
+  return location === "NYC" ?  "Ok, sounds good." : "No go."
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tips){
+  switch(tips){
+    case "generous":
+    return "Thank you so much."
+    case "not as generous" :
+      return "Thank you."
+      case   "thanks for everything":
+        return "Bye."
+  }
 }
